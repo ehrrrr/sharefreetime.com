554edf6a18a4b0abea1624b6996035bfdb91231e
ehrrrr <ehrrrr@gmail.com>
2016-09-25 18:38:39 +0300

    upload and edit picture is added

--- sharefreetime.com/edit_picture.php
+++ sharefreetime.com/edit_picture.php
@@ -1,3 +1,8 @@
+<?php require './includes/connections.php'; ?>
+<?php 
+        session_start();
+        $user = $_SESSION["user_id"];
+?>
 <?php
 	if(isset($_FILES['UploadFileField'])){
 		// Creates the Variables needed to upload the file
@@ -9,6 +14,7 @@
 		
 		// Removes Unwanted Spaces and characters from the files names of the files being uploaded
 		$UploadName = preg_replace("#[^a-z0-9.]#i", "", $UploadName);
+        
 		// Upload File Size Limit 
 		if(($FileSize > 125000)){
 			
@@ -23,18 +29,9 @@
 		}
 	}
 ?>
-<?php require './includes/connections.php'; ?>
-<?php
-    session_start();
-    $user = $_SESSION["user_id"];
-
-    $sql = $db->query("SELECT * FROM users WHERE user_id = '$user'");
-    $result = $sql->fetch_array(MYSQLI_BOTH);
-
-    $_SESSION["UserImage"] = $result['image'];
-?>
 <?php 
-    if(isset($_POST['update_btn'])){
+
+    if(isset($_POST['UploadButton'])){
         
         $updateImage = $mysqli->real_escape_string($UploadName);
         
@@ -65,28 +62,32 @@
 
         <section>
             <div class="container-fluid form">
-                <form  class="form-horizontal" method="post" action="edit_picture.php">
-                        <div class="row">
-                            <?php include "./includes/sidebar.php"?>
-                            <div class="col-md-5 col-sm-8 main-form">
-                               <div class="row">
-                                   <div class="col-sm-12 text-center"><h3>Edit About Me</h3></div>
-                               </div>
-                            <div id="upload_image"></div>
-                            <form action="FileUpload.php" method="post" enctype="multipart/form-data" name="FileUploadForm" id="FileUploadForm">
-                                <label for="UploadFileField"></label>
-                                <input type="file" name="UploadFileField" id="UploadFileField" />
-                                <input type="submit" name="UploadButton" id="UploadButton" value="Upload" />
-                              </form>                                
-                              <button type="submit" name="update_btn" class="btn btn-block button-yg login-button">Update</button>
-                              <div class="login-register">
-                                  <a href="./myprofile.php">Back to my profile</a>
-                              </div>
+                <div class="row">
+                    <?php include "./includes/sidebar.php"?>
+                    <div class="col-md-5 col-sm-8 main-form">
+                       <div class="row">
+                           <div class="col-sm-12 text-center"><h3>Upload Photo</h3></div>
+                       </div>
+
+                        <div class="row fileuploadholder">
+                          <form action="edit_picture.php" method="post" enctype="multipart/form-data" name="FileUploadForm" id="FileUploadForm">
+                            <div class="row">
+                                <div class="form-group">
+                                    <label for="UploadFileField"></label>
+                                    <input type="file" name="UploadFileField" id="UploadFileField" />
+                                </div>
                             </div>
-                            <div class="col-md-3 col-sm-1"></div>
+                            <div id="image-holder"></div>
+                            <div class="row"></div>
+                            <div class="row">
+                                <input type="submit" name="UploadButton" id="UploadButton" value="Upload" />
+                            </div>
+                          </form>
                         </div>
-                </form>
-            
+
+                      </div>
+                    <div class="col-md-3 col-sm-1"></div>
+                </div>
             </div>    
         </section>
 
@@ -95,6 +96,39 @@
         <?php include_once "./includes/footer.php"; ?>
 
     </body>
+    <script>
+        $(document).ready(function() {
+            $("#UploadFileField").on('change', function() {
+              //Get count of selected files
+              var countFiles = $(this)[0].files.length;
+              var imgPath = $(this)[0].value;
+              var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+              var image_holder = $("#image-holder");
+              image_holder.empty();
+              if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+                if (typeof(FileReader) != "undefined") {
+                  //loop for each file selected for uploaded.
+                  for (var i = 0; i < countFiles; i++) 
+                  {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                      $("<img />", {
+                        "src": e.target.result,
+                        "class": "thumb-image"
+                      }).appendTo(image_holder);
+                    }
+                    image_holder.show();
+                    reader.readAsDataURL($(this)[0].files[i]);
+                  }
+                } else {
+                  alert("This browser does not support FileReader.");
+                }
+              } else {
+                alert("Pls select only images");
+              }
+            });
+        });
+    </script>
     <script type="text/javascript">
         $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
     </script>
